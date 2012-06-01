cfae08e8f58da69fe17aba573f190a16b41dfb04
Aaron Xu <greatsnows@gmail.com>
2012-06-01 22:24:54 +0800

    finish progressBar

--- src/overheat/study/Activity15.java
+++ src/overheat/study/Activity15.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
@@ -11,6 +12,7 @@ public class Activity15 extends Activity {
 	private	ProgressBar firstBar = null;
 	private	ProgressBar secondBar = null;
 	private	Button	myButton = null;
+	private int i = 0;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,12 +21,30 @@ public class Activity15 extends Activity {
         firstBar = (ProgressBar)findViewById(R.id.firstBar);
         secondBar = (ProgressBar)findViewById(R.id.secondBar);
         myButton = (Button)findViewById(R.id.myButton);
-    }
-    class ButtonListener implements OnClickListener{
-    	@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
+        myButton.setOnClickListener(new View.OnClickListener() {
 			
-		}
+        	public void onClick(View v) {
+    			// TODO Auto-generated method stub
+    			if(i == 0)
+    			{
+    				firstBar.setVisibility(View.VISIBLE);
+    				secondBar.setVisibility(View.VISIBLE);
+
+    			}
+    			else if (i<firstBar.getMax())
+    			{
+    				firstBar.setProgress(i);
+    				firstBar.setSecondaryProgress(i+10);
+    				secondBar.setProgress(i);
+    			}
+    			else
+    			{
+    				firstBar.setVisibility(View.GONE);
+    				secondBar.setVisibility(View.GONE);
+    			}
+    			i = i+10;
+        	}
+        });
     }
+
 }
